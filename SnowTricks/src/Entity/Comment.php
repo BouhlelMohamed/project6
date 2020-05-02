7282b08d132597f51f8395ac34c0821622c9393a
Bouhlel Mohamed <mohamed.bouhleel@gmail.com>
2020-05-02 16:15:56 +0200

    Update Comment.php

--- SnowTricks/src/Entity/Comment.php
+++ SnowTricks/src/Entity/Comment.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
  */
 class Comment
-{s
+{
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
